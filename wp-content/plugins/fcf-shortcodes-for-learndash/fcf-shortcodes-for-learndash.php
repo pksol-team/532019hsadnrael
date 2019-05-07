25a0cebd52aa4bd68bf340c6123a46dcae63763a
Noman <nomanaadma@gmail.com>
2019-05-07 10:01:07 +0500

    rut removed

--- wp-content/plugins/fcf-shortcodes-for-learndash/fcf-shortcodes-for-learndash.php
+++ wp-content/plugins/fcf-shortcodes-for-learndash/fcf-shortcodes-for-learndash.php
@@ -11,23 +11,65 @@ License: GPL2
 
 add_shortcode('user_meta', 'user_meta_shortcode_handler');
 function user_meta_shortcode_handler($atts,$content=null){
- 
-	if ( !isset( $atts['user_id'] ) ){
-		$user = wp_get_current_user();
-		$atts['user_id'] = $user->ID;
-	}
 
-	$user = new WP_User($atts['user_id']);
- 
-	if ( !$user->exists() ) return;
- 
-	if ( $user->has_prop( $atts['key'] ) ){
-			$value = $atts['user_id']; //$user->get( $atts['key'] );
-	}
- 
-	if (!empty( $value )){
-		return $atts['pre'] . $value . $atts['post'] ;
-	}
- 
-	return;
+	$user_id = get_current_user_id();
+	$rut = get_user_meta($user_id, 'rut', true);
+
+	return 'RUT '.$rut;
+
+}
+
+add_shortcode('user_id', 'user_id_handler');
+function user_id_handler() {
+
+	$user_id = get_current_user_id();
+	return $user_id;
+
+}
+
+add_shortcode('user_profile_image', 'user_profile_image_handler');
+function user_profile_image_handler() {
+	// return '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img style="float: right; " src="?virtual_image=">';
+	return '<img style="float: right; " src="?virtual_image=">';
+}
+
+add_filter( 'init', function( $template ) {
+    if ( isset( $_GET['virtual_image'] ) ) {
+
+		$user_id = get_current_user_id();
+		$image = image_generate(get_avatar_url($user_id));
+
+        die;
+    }
+} );
+
+
+function image_generate($filename) {
+	$image_s = imagecreatefromstring(file_get_contents($filename));
+	$width = imagesx($image_s);
+	$height = imagesy($image_s);
+
+	$newwidth = 250;
+	$newheight = 250;
+
+	$image = imagecreatetruecolor($newwidth, $newheight);
+	imagealphablending($image, true);
+	imagecopyresampled($image, $image_s, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+	$mask = imagecreatetruecolor($newwidth, $newheight);
+
+	$transparent = imagecolorallocate($mask, 25,0,0);
+	imagecolortransparent($mask, $transparent);
+
+	imagefilledellipse($mask, $newwidth/2, $newheight/2, $newwidth, $newheight, $transparent);
+
+	$red = imagecolorallocate($mask, 0, 0, 0);
+	imagecopymerge($image, $mask, 0, 0, 0, 0, $newwidth, $newheight, 100);
+	imagecolortransparent($image, $red);
+	imagefill($image, 0,0,$red);
+
+	header('Content-type: image/png');
+	imagepng($image);
+	imagepnd($image, 'output.png');
+	imagedestroy($image);
 }
